ebb7f35c7aab1defdc830eeae8e4d0068bcb10df
Ted Moke <theodore.moke@inventivhealth.com>
2018-01-07 13:25:11 -0500

    Initial commit

---
+++ content-script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // console.log('Hellow world');
+    var div = document.createElement("div");
+    document.body.appendChild(div);
+    div.innerText = "test123";
+});
+
+var div = document.createElement("div");
+    document.body.appendChild(div);
+    div.innerText = "test123";
+
+    console.log('Hellow');
