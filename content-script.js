00961b37a12d29cf90e8cde7572dde9e6c3f4968
Ted Moke <theodore.moke@inventivhealth.com>
2018-01-14 21:45:08 -0500

    Checks title against unit and abbrevs

--- content-script.js
+++ content-script.js
@@ -17,7 +17,7 @@ var initJetlify = function() {
 
                 for (prod of prodList) {
                     var content = (prod.getElementsByClassName('tile-contents'));
-                    var title = content[0].childNodes[0].getElementsByClassName('name')[0].textContent;
+                    var title = content[0].childNodes[0].getElementsByClassName('name')[0].textContent.toLowerCase();
                     var pricingBlock = content[0].childNodes[1];
 
                     pricePer = prod.getElementsByClassName('price-per')[0];
@@ -28,7 +28,7 @@ var initJetlify = function() {
                         pricingBlock = [pricingBlock.getElementsByClassName('price-std-block')[0].textContent];
                     }
 
-                    calculatePerItem(title, pricingBlock);
+                    calculatePerItem(title, pricingBlock, units);
 
                 }
             }, 200);
@@ -45,7 +45,6 @@ var initJetlify = function() {
     var getExistingUnits = function(list) {
         for (pricePerItem of list) {
             var pricePer = pricePerItem.innerHTML.replace(/[\(\)\s]/g, '').split('/');
-            var amount = Number(pricePer[0].replace(/[^0-9\.-]+/g, ""));
             var unit = pricePer[1].toLowerCase();
 
             if (unitList === undefined) {
@@ -56,9 +55,32 @@ var initJetlify = function() {
 
             return unitList;
         }
-    }
+    };
 
-    var calculatePerItem = function(title, price) {
+    var calculatePerItem = function(title, price, units) {
+        for (unit of units) {
+            if (title.indexOf(unit) >= 0) {
+                console.log('title contains exact');
+            } else {
+                for (unitType of unitKey) {
+                    var unitInKey = unitType.indexOf(unit);
+                    if (unitInKey >= 0) {
+                        for (unitAbbrev of unitType) {
+                            console.log(unitAbbrev);
+                            if (title.indexOf(unitAbbrev) >= 0) {
+                                console.log('title contains similar');
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
         console.log(title, price, units);
-    }
+    };
+
+    var unitKey = [
+        ['count', 'ct', 'cnt'],
+        ['ounces', 'oz', 'ounce']
+    ];
 }();
